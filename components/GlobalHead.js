f4d6760aa801c5ed18f2fce2a2ce347466d76c4b
BKDragon <xiaoyao@live.it>
2024-07-29 20:48:17 +0800

    Update GlobalHead.js

--- components/GlobalHead.js
+++ components/GlobalHead.js
@@ -159,6 +159,7 @@ const GlobalHead = props => {
         </>
       )}
       {children}
+       <script async src="https://fundingchoicesmessages.google.com/i/pub-9916890504173641?ers=1" nonce="qyivJbYKQZ8yjBrZtXGfLg"></script><script nonce="qyivJbYKQZ8yjBrZtXGfLg">(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();</script>
     </Head>
   )
 }
